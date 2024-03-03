4855b7d337a9b4fef1fac83758b6f42406c132b0
MustafaYavas <mustafayavas40@gmail.com>
2024-03-03 12:29:11 +0300

    Add test-util for providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import './App.css';
 import Application from './components/Application/Application';
 import Counter from './components/Counter/Counter';
+import { MuiMode } from './components/Mui/MuiMode';
 import Skills from './components/Skills/Skills';
+import { AppProviders } from './components/providers/AppProviders';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Application /> */}
-      {/* <Skills skills={['HTML', 'CSS', 'JS']} /> */}
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className="App">
+        {/* <Application /> */}
+        {/* <Skills skills={['HTML', 'CSS', 'JS']} /> */}
+        {/* <Counter /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
